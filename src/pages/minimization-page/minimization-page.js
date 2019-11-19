c2293b3b297748950cdf48144566bb1721e74103
Ihor Kulyk <ihor.kulyk34@gmail.com>
2019-11-19 02:49:08 +0200

    prettier src files

--- src/pages/minimization-page/minimization-page.js
+++ src/pages/minimization-page/minimization-page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavigationHeader} from '../../ui-components';
-import {IconButton,IconButtonTypes} from '../../ui-components';
+import {IconButton, IconButtonTypes} from '../../ui-components';
 import {DetailedFunctionDescription} from './components/detailed-function-description';
 import './minimization-page.css';
 
@@ -9,7 +9,9 @@ export default function MinimizationPage(props) {
   return (
     <div className="minimization-page" {...rest}>
       <NavigationHeader>
-        <IconButton iconType={IconButtonTypes.ARROWLEFT} onClick={onBackToMainPsge}></IconButton>
+        <IconButton
+          iconType={IconButtonTypes.ARROWLEFT}
+          onClick={onBackToMainPsge}></IconButton>
       </NavigationHeader>
       <DetailedFunctionDescription id="powell-function"></DetailedFunctionDescription>
     </div>
