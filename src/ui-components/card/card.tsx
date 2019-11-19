006451da01e89f6d8beeaf4ce51c07b35b96d6ff
Ihor Kulyk <ihor.kulyk34@gmail.com>
2019-11-19 02:45:50 +0200

    update types for card and navigation-header components

--- src/ui-components/card/card.tsx
+++ src/ui-components/card/card.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import styles from './card.module.css';
 
-export const Card: React.FC = (
-  props: React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLDivElement>,
-    HTMLDivElement
-  >
-) => {
+export const Card: React.FC<React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>> = props => {
   const {className = '', ...rest} = props;
   return <div className={`${styles.root} ${className}`} {...rest}></div>;
 };
--- src/ui-components/navigation-header/navigation-header.tsx
+++ src/ui-components/navigation-header/navigation-header.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import styles from './navigation-header.module.css';
 
-export const NavigationHeader: React.FC = (
-  props: React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLDivElement>,
-    HTMLDivElement
-  >
-) => {
+export const NavigationHeader: React.FC<React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>> = props => {
   const {className = '', ...rest} = props;
   return <div className={`${styles.root} ${className}`} {...rest}></div>;
 };
