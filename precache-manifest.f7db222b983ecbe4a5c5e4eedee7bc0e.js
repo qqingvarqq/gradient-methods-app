e112ad63436d74f967ee305ff346e634afed91c0
Ihor Kulyk <ihor.kulyk34@gmail.com>
2019-11-27 01:10:40 +0200

    Updates

---
+++ precache-manifest.f7db222b983ecbe4a5c5e4eedee7bc0e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207453ef236672187e244db05fda8b5e",
+    "url": "/gradient-methods-app/16a68a57d6b9d1e4029e.worker.js"
+  },
+  {
+    "revision": "2864d69939ac325c305c0368382d699d",
+    "url": "/gradient-methods-app/index.html"
+  },
+  {
+    "revision": "6619c85960fc58dd5b02",
+    "url": "/gradient-methods-app/static/css/main.1442a42e.chunk.css"
+  },
+  {
+    "revision": "6e9b91360a9d5cbf1ad4",
+    "url": "/gradient-methods-app/static/js/2.7459459f.chunk.js"
+  },
+  {
+    "revision": "6619c85960fc58dd5b02",
+    "url": "/gradient-methods-app/static/js/main.1a013cdb.chunk.js"
+  },
+  {
+    "revision": "816feae5990920473b56",
+    "url": "/gradient-methods-app/static/js/runtime-main.755df07e.js"
+  },
+  {
+    "revision": "857c236f76c14f3b59afccb618a0fddd",
+    "url": "/gradient-methods-app/static/media/penalty-function-1.857c236f.svg"
+  },
+  {
+    "revision": "77d1764b344c2356687f97ba6cec8a21",
+    "url": "/gradient-methods-app/static/media/penalty-function-2.77d1764b.svg"
+  },
+  {
+    "revision": "e9525fb4741c662e21f72bd463e1843a",
+    "url": "/gradient-methods-app/static/media/powell-function.e9525fb4.svg"
+  },
+  {
+    "revision": "dfc14948f7fd3bb6f5a0dc0921771c77",
+    "url": "/gradient-methods-app/static/media/rosenbrock-function.dfc14948.svg"
+  }
+]);
